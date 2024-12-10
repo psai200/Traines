f762cc8485c70f4868240f483fef19afb66a2584
psai200 <saiponnamanda25@gmail.com>
2024-12-10 13:15:59 +0530

    java

---
+++ Traines/src/Array/Demo.java
@@ -0,0 +1,19 @@
+package Array;
+
+import java.lang.reflect.Array;
+
+public class Demo {
+	void meth1() {
+	int arr1[];
+	arr1 =new int[5];
+	int arr2[]=new int[5];
+	int arr3[]= {10,20,30,11,5};
+	int arr4[]= {11,12,13,14,15};
+	System.out.println("array length:"+ arr1.length);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Demo().meth1();
+	}
+
+}
